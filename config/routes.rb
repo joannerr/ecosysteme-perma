81b3a4432e91480c945aaf9121ab44835fd405f2
Joanne <jonane_k3ly@hotmail.fr>
2018-09-13 12:47:39 +0200

    front static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
 	root to: "static_pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	get '/presentation', to: 'static_pages#presentation'
+	get '/partenaires', to: 'static_pages#partners'
+	get '/profil', to: 'static_pages#profile'
+	get '/fondation', to: 'static_pages#foundation'
 end
